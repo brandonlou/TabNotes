952589f9d899928ecaf255e43baef540d6f77cd9
Brandon Lou <brandonlou@users.noreply.github.com>
2016-12-22 19:30:29 -0800

    Added name in settings (#3)

--- js/script.js
+++ js/script.js
@@ -5,25 +5,12 @@ editor.render();
 var timer = setInterval(save, 100); // Update every 100ms
 var storedNotes;
 
-var d = new Date();
-var h = d.getHours();
-var name;
-chrome.storage.sync.get(['userName'], function(data) {
-  name = data.userName;
-  if(name == "undefined") name = "";
-});
+/* Get time and print title message accordingly */
+var d = new Date(),
+    h = d.getHours(),
+    name;
 
-if (h < 4) { // night
-  $('#title').text("Good night " + name);
-} else if (h < 12) { // morning
-  $('#title').text("Good morning " + name);
-} else if (h < 17) { // afternoon
-  $('#title').text("Good aftenoon " + name);
-} else if (h < 10) { // evening
-  $('#title').text("Good evening " + name);
-} else { // night
-  $('#title').text("Good night " + name);
-}
+updateMessage();
 
 /* First time: Gets stored notes and displays it */
 chrome.storage.sync.get(['notes'], function(data) {
@@ -45,4 +32,46 @@ function save() {
   if(editor.codemirror.getValue() != storedNotes) {
     chrome.storage.sync.set({'notes': editor.codemirror.getValue()});
   }
+}
+
+var settingsRendered = false;
+
+$("#settingsButton").click(function() {
+  if(settingsRendered) {
+    $("#settingsText").text("");
+    settingsRendered = false;
+  } else {
+    $.ajax({url: "templates/settings.html", success: function(result){
+        $("#settingsText").html(result);
+        $("#nameInput").val(name);
+        settingsRendered = true;
+    }});
+  }
+});
+
+$( document ).ajaxComplete(function() {
+  $("#saveSettings").click(function() {
+    chrome.storage.local.set({'userName': $("#nameInput").val()});
+    location.reload();
+    updateMessage();
+    $("#settingsText").text("");
+  });
+});
+
+function updateMessage() {
+  chrome.storage.local.get(['userName'], function(data) {
+    name = data.userName;
+    if(name == "undefined") name = "World";
+    if (h < 4) { // night
+      $('#title').text("Good night " + name);
+    } else if (h < 12) { // morning
+      $('#title').text("Good morning " + name);
+    } else if (h < 17) { // afternoon
+      $('#title').text("Good aftenoon " + name);
+    } else if (h < 10) { // evening
+      $('#title').text("Good evening " + name);
+    } else { // night
+      $('#title').text("Good night " + name);
+    }
+  });
 }
